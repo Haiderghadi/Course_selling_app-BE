7b91917d007dc328aa2f0537aa8b724a30970753
Haiderghadi <haiderghadi07@gmail.com>
2024-03-08 17:28:16 +0530

    files uploaded

---
+++ routes/admin.js
@@ -0,0 +1,50 @@
+const { Router } = require("express");
+const adminMiddleware = require("../middleware/admin");
+const { Admin, Course } = require("../db");
+const router = Router();
+
+// Admin Routes
+router.post("/signup", async (req, res) => {
+  // Implement admin signup logic
+  const username = req.body.username;
+  const password = req.body.password;
+
+  await Admin.create({
+    username,
+    password,
+  });
+  res.json({
+    msg: "admin created!",
+  });
+});
+
+router.post("/courses", adminMiddleware, async (req, res) => {
+  // Implement course creation logic
+  const title = req.body.title;
+  const description = req.body.description;
+  const imageLink = req.body.imageLink;
+  const price = req.body.price;
+
+  const NewCourse = await Course.create({
+    title,
+    description,
+    imageLink,
+    price,
+  });
+
+  res.json({
+    msg: "Course Created !",
+    CourseId: NewCourse._id,
+  });
+});
+
+router.get("/courses", adminMiddleware, async (req, res) => {
+  // Implement fetching all courses logic
+  const response = await Course.find({});
+
+  res.json({
+    course: response,
+  });
+});
+
+module.exports = router;
